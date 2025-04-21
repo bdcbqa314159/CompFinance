fd4d663e8ff8ee70ad88a67561a6bfec2f3747d8
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2025-04-21 23:30:32 +0100

    correcting some includes - still

--- libraries/aad/includes/AADTape.h
+++ libraries/aad/includes/AADTape.h
@@ -19,163 +19,131 @@ As long as this comment is preserved at the top of the file
 //  AAD implementation of chapter 10
 //  (With multi-dimensional additions of chapter 14)
 
-//  Implementation of the Tape 
+//  Implementation of the Tape
 
 //  Unchanged for AADET of chapter 15
 
-#include "blocklist.h"
 #include "AADNode.h"
+#include "blocklist.h"
 
-constexpr size_t BLOCKSIZE  = 16384;		//	Number of nodes
-constexpr size_t ADJSIZE    = 32768;		//	Number of adjoints
-constexpr size_t DATASIZE   = 65536;		//	Data in bytes
+constexpr size_t BLOCKSIZE = 16384; //	Number of nodes
+constexpr size_t ADJSIZE = 32768;   //	Number of adjoints
+constexpr size_t DATASIZE = 65536;  //	Data in bytes
 
-class Tape
-{
-	//	Working with multiple results / adjoints?
-	static bool							multi;
+class Tape {
+  //	Working with multiple results / adjoints?
+  static bool multi;
 
-	//  Storage for adjoints in multi-dimensional case (chapter 14)
-    blocklist<double, ADJSIZE>			myAdjointsMulti;
-    
-	//  Storage for derivatives and child adjoint pointers
-	blocklist<double, DATASIZE>			myDers;
-	blocklist<double*, DATASIZE>		myArgPtrs;
+  //  Storage for adjoints in multi-dimensional case (chapter 14)
+  blocklist<double, ADJSIZE> myAdjointsMulti;
 
-    //  Storage for the nodes
-	blocklist<Node, BLOCKSIZE>		    myNodes;
+  //  Storage for derivatives and child adjoint pointers
+  blocklist<double, DATASIZE> myDers;
+  blocklist<double *, DATASIZE> myArgPtrs;
 
-	//	Padding so tapes in a vector don't interfere
-    char                                myPad[64];
+  //  Storage for the nodes
+  blocklist<Node, BLOCKSIZE> myNodes;
 
-    friend auto setNumResultsForAAD(const bool, const size_t);
-    friend struct numResultsResetterForAAD;
-	friend class Number;
+  //	Padding so tapes in a vector don't interfere
+  char myPad[64];
+
+  friend auto setNumResultsForAAD(const bool, const size_t);
+  friend struct numResultsResetterForAAD;
+  friend class Number;
 
 public:
+  //  Build note in place and return a pointer
+  //	N : number of childs (arguments)
+  template <size_t N> Node *recordNode() {
+    //  Construct the node in place on tape
+    Node *node = myNodes.emplace_back(N);
 
-    //  Build note in place and return a pointer
-	//	N : number of childs (arguments)
-    template <size_t N>
-    Node* recordNode()
-    {
-        //  Construct the node in place on tape
-        Node* node = myNodes.emplace_back(N);
-        
-        //  Store and zero the adjoint(s)
-        if (multi)
-        {
-            node->pAdjoints = myAdjointsMulti.emplace_back_multi(Node::numAdj);
-            fill(node->pAdjoints, node->pAdjoints + Node::numAdj, 0.0);
-        }
-
-		//	Store the derivatives and child adjoint pointers unless leaf
-		if constexpr(N>0)
-		{
-			node->pDerivatives = myDers.emplace_back_multi<N>();
-			node->pAdjPtrs = myArgPtrs.emplace_back_multi<N>();
-
-		}
-
-        return node;
+    //  Store and zero the adjoint(s)
+    if (multi) {
+      node->pAdjoints = myAdjointsMulti.emplace_back_multi(Node::numAdj);
+      fill(node->pAdjoints, node->pAdjoints + Node::numAdj, 0.0);
     }
 
-    //  Reset all adjoints to 0
-	void resetAdjoints()
-	{
-		if (multi)
-		{
-			myAdjointsMulti.memset(0);
-		}
-		else
-		{
-			for (Node& node : myNodes)
-			{
-				node.mAdjoint = 0;
-			}
-		}
-	}
-
-    //  Clear
-    void clear()
-    {
-        myAdjointsMulti.clear();
-		myDers.clear();
-		myArgPtrs.clear();
-        myNodes.clear();
+    //	Store the derivatives and child adjoint pointers unless leaf
+    if constexpr (N > 0) {
+      node->pDerivatives = myDers.emplace_back_multi<N>();
+      node->pAdjPtrs = myArgPtrs.emplace_back_multi<N>();
     }
 
-    //  Rewind
-    void rewind()
-    {
+    return node;
+  }
+
+  //  Reset all adjoints to 0
+  void resetAdjoints() {
+    if (multi) {
+      myAdjointsMulti.memset(0);
+    } else {
+      for (Node &node : myNodes) {
+        node.mAdjoint = 0;
+      }
+    }
+  }
+
+  //  Clear
+  void clear() {
+    myAdjointsMulti.clear();
+    myDers.clear();
+    myArgPtrs.clear();
+    myNodes.clear();
+  }
+
+  //  Rewind
+  void rewind() {
 
 #ifdef _DEBUG
 
-        //  In debug mode, always wipe
-        //      makes it easier to identify errors
+    //  In debug mode, always wipe
+    //      makes it easier to identify errors
 
-		clear();
+    clear();
 
 #else
-        //  In release mode, rewind and reuse
+    //  In release mode, rewind and reuse
 
-		if (multi)
-		{
-			myAdjointsMulti.rewind();
-		}
-		myDers.rewind();
-		myArgPtrs.rewind();
-		myNodes.rewind();
+    if (multi) {
+      myAdjointsMulti.rewind();
+    }
+    myDers.rewind();
+    myArgPtrs.rewind();
+    myNodes.rewind();
 
 #endif
+  }
 
+  //  Set mark
+  void mark() {
+    if (multi) {
+      myAdjointsMulti.setmark();
     }
+    myDers.setmark();
+    myArgPtrs.setmark();
+    myNodes.setmark();
+  }
 
-    //  Set mark
-    void mark()
-    {
-        if (multi)
-        {
-            myAdjointsMulti.setmark();
-        }
-		myDers.setmark();
-		myArgPtrs.setmark();
-		myNodes.setmark();
+  //  Rewind to mark
+  void rewindToMark() {
+    if (multi) {
+      myAdjointsMulti.rewind_to_mark();
     }
+    myDers.rewind_to_mark();
+    myArgPtrs.rewind_to_mark();
+    myNodes.rewind_to_mark();
+  }
 
-    //  Rewind to mark
-    void rewindToMark()
-    {
-        if (multi)
-        {
-            myAdjointsMulti.rewind_to_mark();
-        }
-		myDers.rewind_to_mark();
-		myArgPtrs.rewind_to_mark();
-		myNodes.rewind_to_mark();
-    }
+  //  Iterators
 
-    //  Iterators
-    
-    using iterator = blocklist<Node, BLOCKSIZE>::iterator;
+  using iterator = blocklist<Node, BLOCKSIZE>::iterator;
 
-    auto begin()
-    {
-        return myNodes.begin();
-    }
+  auto begin() { return myNodes.begin(); }
 
-    auto end()
-    {
-        return myNodes.end();
-    }
+  auto end() { return myNodes.end(); }
 
-    auto markIt()
-    {
-        return myNodes.mark();
-    }
+  auto markIt() { return myNodes.mark(); }
 
-    auto find(Node* node)
-    {
-        return myNodes.find(node);
-    }
+  auto find(Node *node) { return myNodes.find(node); }
 };
